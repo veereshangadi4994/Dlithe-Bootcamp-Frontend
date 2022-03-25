059604e17917363afcd6e58eb2222870dcf213e7
yourusername <veereshangadi4994@gmail.com>
2022-03-25 14:04:35 +0530

    commit

---
+++ external.js
@@ -0,0 +1,13 @@
+function message2() {
+    //alert("I am an External JS File");
+    var age = 20;
+    var nm = "Chetan";
+    var ab25 = true;
+    var height = 1.59;
+    DA = null;
+    document.write("Name: " + nm + "<br>");
+    document.write("Age:" + age + "<br>");
+    document.write("Below 25:" + ab25 + "<br>");
+    document.write("Height:" + height + "<br>");
+    document.write("Differently abled:" + DA + "<br>");
+}
